de3a8054ba7f4b410bf131b9f1fdc43cfadc9f94
Syll22 <syll_22@yahoo.com>
2016-12-08 18:22:54 +0200

    Staircase algorithm done

--- HackerRank/Staircase/Staircase/Program.cs
+++ HackerRank/Staircase/Staircase/Program.cs
@@ -10,6 +10,34 @@ namespace Staircase
     {
         static void Main(string[] args)
         {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1 ; i <= n; i++)
+            {
+                Console.WriteLine(Space(n-i) + Pound(n-(n-i)));
+            }
+            
+            Console.ReadKey(true);
+        }
+
+        static string Space (int a)
+        {
+            string s = "";
+            for (int i = 1; i <= a; i++)
+            {
+                s = s + " ";
+            }
+            return s;
+        }
+
+        static string Pound (int b)
+        {
+            string t = "";
+            for (int i = 1; i <= b; i++)
+            {
+                t = t + "#";
+            }
+            return t;
         }
     }
 }
