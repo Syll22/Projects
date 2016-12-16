15d2bd8475ee27327be4b820b90dc01f491368c7
Syll22 <syll_22@yahoo.com>
2016-12-16 19:23:27 +0200

    Designer PDF algorith start

---
+++ HackerRank/DesignerPDFViewer/DesignerPDFViewer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignerPDFViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] h_temp = Console.ReadLine().Split(' ');
+            int[] h = Array.ConvertAll(h_temp, Int32.Parse);
+            string word = Console.ReadLine();
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            int maxH = 0;
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            
+            for (int i = 0; i<alphabet.Length-1; i++)
+                dict.Add(alphabet[i], h[i]);
+
+            maxH = Math.Max(dict.TryGetValue(word[0]));
+        }
+    }
+}
