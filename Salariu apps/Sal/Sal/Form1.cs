6fe88ba2f8538ca4bce614aa3297eac3c9139f10
Syll22 <syll_22@yahoo.com>
2017-02-08 12:27:46 +0200

    Update sal app. new calculation

--- Salariu apps/Sal/Sal/Form1.cs
+++ Salariu apps/Sal/Sal/Form1.cs
@@ -26,25 +26,29 @@ namespace Sal
             if (radioButtonScutit.Checked && textBoxNet.Text != "")
             {
                 net = Double.Parse(textBoxNet.Text);
-                brut = net > 11193 ? (net + 1408) / 0.94 : net / 0.835;
+                //brut = net > 11193 ? (net + 1408) / 0.94 : net / 0.835; Calcul vechi cu palfonarea CAS
+                brut = net / 0.835 ;
                 textBoxBrut.Text = Convert.ToString(brut);
             }
             else if (radioButtonScutit.Checked && textBoxBrut.Text != "")
             {
                 brut = Double.Parse(textBoxBrut.Text);
-                net = brut > 13405 ? (brut * 0.94) - 1408 : brut * 0.835;
+                //net = brut > 13405 ? (brut * 0.94) - 1408 : brut * 0.835; Calcul vechi cu palfonarea CAS
+                net = brut * 0.835 ;
                 textBoxNet.Text = Convert.ToString(net);
             }
             else if (radioButtonNescutit.Checked && textBoxNet.Text != "")
             {
                 net = Double.Parse(textBoxNet.Text);
-                brut = net > 9402 ? (net + 1183) / 0.7896 : net / 0.835 / 0.84;
+                //brut = net > 9402 ? (net + 1183) / 0.7896 : net / 0.835 / 0.84; Calcul vechi cu palfonarea CAS
+                brut = net / 0.835 / 0.84;
                 textBoxBrut.Text = Convert.ToString(brut);
             }
             else if (radioButtonNescutit.Checked && textBoxBrut.Text != "")
             {
                 brut = Double.Parse(textBoxBrut.Text);
-                net = brut > 13405 ? (brut * 0.7896) - 1183 : brut * 0.7014;
+                //net = brut > 13405 ? (brut * 0.7896) - 1183 : brut * 0.7014; Calcul vechi cu palfonarea CAS
+                net = brut * 0.835 * 0.84;
                 textBoxNet.Text = Convert.ToString(net);
             }
         }
