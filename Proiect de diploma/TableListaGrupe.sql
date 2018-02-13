affcc3bddc8d7c4da9a0eaceaf09ed8cc2dabb30
Silviu - work laptop <sserigeanu@luxoft.com>
2018-02-13 12:35:53 +0200

    Proiect diploma start.

---
+++ Proiect de diploma/TableListaGrupe.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE ListaGrupe
+(
+	[IdGrupa] INT NOT NULL PRIMARY KEY, 
+    [IdSpecializare] INT NOT NULL, 
+    [Enabled] BIT NOT NULL DEFAULT 1
+)
---
+++ Proiect de diploma/TableListaStudenti.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE ListaStudenti
+(
+	[NumarMatricol] INT NOT NULL PRIMARY KEY, 
+    [Nume] NVARCHAR(MAX) NOT NULL, 
+    [Grupa] INT NOT NULL, 
+    [Enabled] BIT NOT NULL DEFAULT 1 
+)
