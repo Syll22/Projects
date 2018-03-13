2b98c4956eceff33220866c391ee2a600d2ae4fa
Silviu - work laptop <sserigeanu@luxoft.com>
2018-03-13 17:03:42 +0200

    Added checker for empty textbox for Proffesors and Students. Added example app WpFApplication.

--- Proiect de diploma/CatalogApp/CatalogApp/MainWindow.xaml.cs
+++ Proiect de diploma/CatalogApp/CatalogApp/MainWindow.xaml.cs
@@ -61,6 +61,10 @@ namespace CatalogApp
                 txtNumeProfesor.Text = dataRowView["NumeProfesor"].ToString();
                 txtPrenumeProfesor.Text = dataRowView["PrenumeProfesor"].ToString();
                 txtId.Text = dataRowView["IdProfesor"].ToString();
+                btnAddProfesor.IsEnabled = false;
+                btnUpdateProfesor.IsEnabled = true;
+                btnDeleteProfesor.IsEnabled = true;
+
             }
         }
 
@@ -113,22 +117,44 @@ namespace CatalogApp
             finally
             {
                 conn.Close();
-                status_conn.Background = Brushes.Red;
-
                 resetProfesori();
+                status_conn.Background = Brushes.Red;
             }
         }
 
         private void btnAddProfesor_Click(object sender, RoutedEventArgs e)
         {
+            if (txtNumeProfesor.Text == "")
+            {
+                MessageBox.Show("Nu ati introdus numele profesorului!");
+                txtNumeProfesor.Focus();
+                return;
+            }
+            if (txtPrenumeProfesor.Text == "")
+            {
+                MessageBox.Show("Nu ati introdus prenumele profesorului!");
+                txtPrenumeProfesor.Focus();
+                return;
+            }
             this.AUDProfesori(0);
-            resetProfesori();
         }
 
         private void btnUpdateProfesor_Click(object sender, RoutedEventArgs e)
         {
+            if (txtNumeProfesor.Text == "")
+            {
+                MessageBox.Show("Nu ati introdus numele profesorului!");
+                txtNumeProfesor.Focus();
+                return;
+            }
+            if (txtPrenumeProfesor.Text == "")
+            {
+                MessageBox.Show("Nu ati introdus prenumele profesorului!");
+                txtPrenumeProfesor.Focus();
+                return;
+            }
+
             this.AUDProfesori(1);
-            resetProfesori();
         }
 
         private void btnDeleteProfesor_Click(object sender, RoutedEventArgs e)
@@ -213,6 +239,10 @@ namespace CatalogApp
             txtNumeProfesor.Text = "";
             txtPrenumeProfesor.Text = "";
             txtId.Text = "";
+            btnAddProfesor.IsEnabled = true;
+            btnUpdateProfesor.IsEnabled = false;
+            btnDeleteProfesor.IsEnabled = false;
+
         }
 
         // Studenti --------------------------------------------------------------------------------------------------------
@@ -239,5 +269,125 @@ namespace CatalogApp
         {
             updateDataGrid_Studenti();
         }
+
+        private void dgr_studenti_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dataGrid = sender as DataGrid;
+            DataRowView dataRowView = dataGrid.SelectedItem as DataRowView;
+            if (dataRowView != null)
+            {
+                txtNumeStudent.Text = dataRowView["NumeStudent"].ToString();
+                txtPrenumeStudent.Text = dataRowView["PrenumeStudent"].ToString();
+                txtNumarMatricol.Text = dataRowView["NumarMatricol"].ToString();
+                //cbxStuGrupa.Text = dataRowView["IdGrupa"].ToString();
+                btnAddStudent.IsEnabled = false;
+                btnUpdateStudent.IsEnabled = true;
+                btnDeleteStudent.IsEnabled = true;
+
+            }
+
+        }
+
+        private void AUDStudenti(int pOperatie)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = Properties.Settings.Default.ConnString;
+            conn.Open();
+            status_conn.Background = Brushes.Green;
+
+            String msg = "";
+            String sql = "";
+
+            switch (pOperatie)
+            {
+                case 0:
+                    sql = "INSERT INTO ListaStudenti(NumeStudent, PrenumeStudent) " +
+                        "VALUES('" + txtNumeStudent.Text + "', '" + txtPrenumeStudent.Text + "')";
+                    msg = "Datele despre Student au fost adaugate cu suuces.";
+                    break;
+                case 1:
+                    sql = "UPDATE ListaStudenti SET NumeStudent='" + txtNumeStudent.Text + "', PrenumeStudent='" + txtPrenumeStudent.Text + "' WHERE NumarMatricol=" + txtNumarMatricol.Text;
+                    msg = "Datele despre Student au fost actualizate.";
+                    break;
+                case 2:
+                    sql = "DELETE FROM ListaStudenti WHERE NumarMatricol=" + txtNumarMatricol.Text;
+                    msg = "Datele despre student au fost sterse.";
+                    break;
+            }
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+
+            try
+            {
+
+                int n = cmd.ExecuteNonQuery();
+                if (n > 0)
+                {
+                    MessageBox.Show(msg);
+                    this.updateDataGrid_Studenti();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+                status_conn.Background = Brushes.Red;
+
+                resetStudenti();
+            }
+        }
+
+        private void btnAddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNumeStudent.Text == "")
+            {
+                MessageBox.Show("Nu ati introdus numele studentului!");
+                txtNumeStudent.Focus();
+                return;
+            }
+            if (txtPrenumeStudent.Text == "")
+            {
+                MessageBox.Show("Nu ati introdus prenumele studentului!");
+                txtPrenumeStudent.Focus();
+                return;
+            }
+            AUDStudenti(0);
+        }
+
+        private void btnUpdateStudent_Click(object sender, RoutedEventArgs e)
+        {
+            AUDStudenti(1);
+        }
+
+        private void btnDeleteStudent_Click(object sender, RoutedEventArgs e)
+        {
+            AUDStudenti(2);
+        }
+
+        private void btnResetStudent_Click(object sender, RoutedEventArgs e)
+        {
+            resetStudenti();
+        }
+
+        private void resetStudenti()
+        {
+            txtNumeStudent.Text = "";
+            txtPrenumeStudent.Text = "";
+            txtNumarMatricol.Text = "";
+            cbxStuGrupa.Text = "";
+            btnAddStudent.IsEnabled = true;
+            btnUpdateStudent.IsEnabled = false;
+            btnDeleteStudent.IsEnabled = false;
+
+        }
+
+        // Grupe --------------------------------------------------------------------------------------------------------
+
     }
 }
